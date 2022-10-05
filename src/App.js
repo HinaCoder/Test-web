e4e452d6eb8068121d532edbdb4f308943a91db1
HinaCoder <84410202+HinaCoder@users.noreply.github.com>
2022-10-05 12:59:29 +0530

    V 1.1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React form me... its the best you know na.....
         </a>
       </header>
     </div>
